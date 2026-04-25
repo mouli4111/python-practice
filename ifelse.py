a3f76137065bc1e1135961e73e89e2370606dd09
mouli4111 <moulisri0304@gmail.com>
2026-04-25 16:44:06 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,36 @@
+# a = int(input("enter your age: "))
+# print("your age is:", a)
+
+# if(a>18):
+#     print("you can drive")
+#     print("yes")
+# else:
+#     print("you cannot drive")
+#     print("NO")    
+
+# appleprice = 10
+# budget = 200
+# if(budget - appleprice >50 ):
+#     print("alexa, add 1 kg apple to the cart")
+# elif(budget - appleprice > 70):
+#     print("its ok you can buy apples")    
+# else:
+#     print("alexa, do not add apples to the cart.") 
+
+num = int(input("enter the value of num: "))
+if (num > 0):
+    print("number is positive")
+
+elif (num == 0):
+    print("enter the number is zero")
+
+elif (num == -8989):
+    print("number is special")   
+
+else:
+    print("number is negative")
+
+print("I am so happy now")
+
+
+    

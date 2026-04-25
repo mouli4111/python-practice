40a2eabc79ccc68073be5194f645f2bdebd21545
mouli4111 <moulisri0304@gmail.com>
2026-04-25 16:49:29 +0530

    Add files via upload

---
+++ examplesloops.py
@@ -0,0 +1,22 @@
+# covert temperature parameters
+# covert the celcius to faranhite
+
+float = (input("enter the temperature in celcius:"))
+
+def farenhite(a):
+    return((a*9/5)+32)
+print(farenhite)
+
+#convert the temperature from farenhite to cdelcius
+
+float = (input("enter the temperature"))
+def celcius(a):
+    return((a*9/5)-32)
+
+# find the simple interest
+p = (input("enter the value p:"))
+r = (input("enter the vlaue r:"))
+t = (input("enter the value t:"))
+def simple_interest(p,r,t):
+    return((p*r*t)/100)
+print(simple_interest(p,r,t))

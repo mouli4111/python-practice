414f0577420f9131896ca6c6e574b4ac4de64463
mouli4111 <moulisri0304@gmail.com>
2026-04-25 16:55:48 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,22 @@
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+a = int(input("enter the first value:"))
+b = int(input("enter the second value:"))
+print("addition", (a+b))
+print("substraction", (a-b))
+print("multiplication", (a*b))
+print("devide", (a/b))
+print("remainder", (a//b))
+print("exponential", (a**b))
+print("remainder", (a//b)) #floor devision operator
+
+#input function
+a = input("enter your name:")
+print("my name is:", a)
+
+x = input("enter first number: ")
+y = input("enter the second number: ")
+print(int(x) + int(y))

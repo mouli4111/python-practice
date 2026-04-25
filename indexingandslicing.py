cac8f797d10bacb4ed25d425e843f32dae4034ea
mouli4111 <moulisri0304@gmail.com>
2026-04-25 16:50:38 +0530

    Add files via upload

---
+++ indexingandslicing.py
@@ -0,0 +1,17 @@
+soft_skills = ("communication", "courtesy", "flexibility", "integrity", "responsibility")
+print(soft_skills[0])
+print(soft_skills[-1])
+
+#Slicing
+
+soft_skills = ("communication", "courtesy", "flexibility", "integrity", "responsibility")
+print(soft_skills[1:4])
+print(soft_skills[:4])
+print(soft_skills[:])
+print(soft_skills[::2])
+print(soft_skills[::-1])
+
+#index and count
+
+soft_skills = ("communication", "courtesy", "flexibility", "integrity", "responsibility")
+print(soft_skills.index("integrity"))

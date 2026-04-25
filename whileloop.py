c9a63625af9a09a3319772c1c5f6b33504ff940e
mouli4111 <moulisri0304@gmail.com>
2026-04-25 16:57:11 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,18 @@
+# i = 0
+# while (i <= 3):
+    # print(i)
+    # i = i+1
+# print("done with the loop")
+# i = int(input("enter the number: "))
+# while (i <= 30):
+    # i = int(input("enter the number: "))
+    # print(i)
+# 
+# print("done with the loop")   
+# 
+count = 50
+while (count > 0):
+    print(count)
+    count = count-2
+else:
+    print("counter is 0")

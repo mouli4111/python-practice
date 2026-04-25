fd4319fd14d86f8b84e68ce4ef21bf25edfbdaf0
mouli4111 <moulisri0304@gmail.com>
2026-04-25 16:57:32 +0530

    Add files via upload

---
+++ variableanddatatype.py
@@ -0,0 +1,97 @@
+# first python code  
+# print("hello world")
+# print("my age is 19")
+# print("my name is mouli" , "my age is 19")
+# print(23)
+# print(35+6)
+#varibale
+# name = "mouli"
+# age = 19 
+# price = 25.66
+# print(name)
+# print(price)
+# print(age)
+# 
+# colon 
+# name = "mouli"
+# age = 19
+# print("my name is :", name)
+# print("my age is :", 19)
+# 
+# identifier
+# name = "mouli"
+# age =  19
+# price = 28.98
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# 
+# none and boolean variable
+# age = 23
+# old = False
+# a = None 
+# print(type(age))
+# print(type(old))
+# print(type(a))
+# 
+#arithematic operators
+# a = 2
+# b = 5
+# sum = (a+b)
+# sub = (a-b)
+# mul = (a*b)
+# div = (a/b)
+# modulus = (a%b)
+# devide = (a**b)
+# print(sum)
+# print(sub)
+# print(mul)
+# print(div)
+# print(modulus)
+# print(devide)
+
+# realtional/comparison-operators (use to compare the values)
+# a = 50
+# b = 20
+# print(a == b)   # EQUAL to
+# print(a != b)   # notequal to 
+# print(a >= b)   # geater tha equal to
+# print(a <= b)   # less than equals to
+# print(a > b)    # greater tha
+# print(a < b)    # less than
+
+# assingnment operators (use to assign the values) 
+# num = 10
+# num += 10
+# print("num:", num)
+# sub = 10
+# sub -= 10
+# print("sub:", sub)
+# mul = 10
+# mul *= 3
+# print("mul:", mul)
+# div = 20
+# div /= 2
+# print("div:", div)
+# mod = 20
+# mod %= 2
+# print("mod", mod)
+# ass = 10
+# ass **= 2
+# print("ass", ass)
+
+#logical operator (NOT(give reverse value like true ka false))
+# a = 10
+# b = 20
+# print(not False)
+# print(not (a>b))
+# 
+#AND OPERATOR (Tabhi trie value dega jab dono vlaue true ho ek bhi False means False)
+# val1 = True
+# val2 = False
+# print("and operator:", val1 and val2)
+
+# OR operator(give the ouput "true", if any ine is true)
+# print("or operator:", val1 or val2)
+# 
+# print("not opeartor", (val1>val2))

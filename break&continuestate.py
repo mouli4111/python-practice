b2d6a955e7d582c8b0f0ce243770704796998e62
mouli4111 <moulisri0304@gmail.com>
2026-04-25 16:48:28 +0530

    Add files via upload

---
+++ break&continuestate.py
@@ -0,0 +1,11 @@
+for i in range(12):
+    if i == 10:
+        break
+    print("5 X", i+1, "=", 5*i)
+print("bas khel khatam")   
+
+for i in range(12):
+    if i == 8:
+        print("skip the iteration")
+        continue
+    print("5 X", i, "=", 5*1)

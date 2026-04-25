2b530775cb418ac0557e4d8656eaea5c23742d41
mouli4111 <moulisri0304@gmail.com>
2026-04-25 16:56:44 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,27 @@
+name = "mouli"
+friend = "khushi"
+anotherfriend = "indira"
+apple = 'he said "i am so strong i can do this" '
+print("hello, " + name)
+print(apple)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+print("lets use a for loop/n")
+for character in name:
+    print(character)
+#lets print for apple
+
+print("lets print for loop apple/n") 
+for character in apple:
+    print(character)   
+
+# <b>string slicing
+
+names = "harry, mouli"
+print(names[0:5])   
+print(l[name]) 

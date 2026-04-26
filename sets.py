d350119b8adf49f8cf38f54a92bc6814ebc5e319
mouli4111 <moulisri0304@gmail.com>
2026-04-26 14:40:09 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,32 @@
+# s1 = {'a', 'b', 'd', 'f', 'g', 'k'}
+# s2 = {'o', 'a', 'b', 'j', 'k', 'l'}
+# s3 = s1.union(s2)
+# s4 = s1-s2
+# s5 = s2-s1
+# s6 = s1.intersection(s2)
+# s7 = s2.symmetric_difference(s1)
+# print(s3)
+# print(s4)
+# print(s5)
+# print(s6)
+# print(s7)
+
+
+# A = {1, 2, 3, 4, 5,}
+# B = {6, 7, 8, 9, 0}
+# s1 = A.union(B)
+# s2 = A-B
+# s3 = B-A
+# s4 = A.intersection(B)
+# s5 = B.symmetric_difference(A)
+# print(s1)
+# print(s2)
+# print(s3)
+# print(s4)
+# print(s5)
+
+
+students1 = {"Mouli", "indira", "yashi", "gandhi"}
+students2 = {"Mouli", "khushi", "garima", "ayyush"}
+s1 = students1.intersection(students2)
+print(s1)
